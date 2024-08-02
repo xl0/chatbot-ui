abc6e3d9c07e5558f846dc90a33096aeeb5ccd4e
Alexey <alexey.zaytsev@gmail.com>
2024-08-01 21:02:07 -0500

    Add GPT 4o-mini

--- lib/models/llm/openai-llm-list.ts
+++ lib/models/llm/openai-llm-list.ts
@@ -3,7 +3,23 @@ import { LLM } from "@/types"
 const OPENAI_PLATORM_LINK = "https://platform.openai.com/docs/overview"
 
 // OpenAI Models (UPDATED 1/25/24) -----------------------------
+
 const GPT4o: LLM = {
+  modelId: "gpt-4o-mini",
+  modelName: "GPT-4o Mini",
+  provider: "openai",
+  hostedId: "gpt-4o-mini",
+  platformLink: OPENAI_PLATORM_LINK,
+  imageInput: true,
+  pricing: {
+    currency: "USD",
+    unit: "1M tokens",
+    inputCost: 5,
+    outputCost: 15
+  }
+}
+
+const GPT4oMini: LLM = {
   modelId: "gpt-4o",
   modelName: "GPT-4o",
   provider: "openai",
@@ -13,8 +29,8 @@ const GPT4o: LLM = {
   pricing: {
     currency: "USD",
     unit: "1M tokens",
-    inputCost: 5,
-    outputCost: 15
+    inputCost: 0.15,
+    outputCost: 0.6
   }
 }
 
@@ -83,6 +99,7 @@ const GPT3_5Turbo: LLM = {
 
 export const OPENAI_LLM_LIST: LLM[] = [
   GPT4o,
+  GPT4oMini,
   GPT4Turbo,
   GPT4Vision,
   GPT4,
