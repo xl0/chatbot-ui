c1a852ee5f68643c669dbdd447a6f767cebdb671
Alexey <alexey.zaytsev@gmail.com>
2024-08-01 21:02:07 -0500

    Add GPT 4o-mini

--- types/llms.ts
+++ types/llms.ts
@@ -11,6 +11,7 @@ export type LLMID =
 // OpenAI Models (UPDATED 5/13/24)
 export type OpenAILLMID =
   | "gpt-4o" // GPT-4o
+  | "gpt-4o-mini" // GPT-4o Mini
   | "gpt-4-turbo-preview" // GPT-4 Turbo
   | "gpt-4-vision-preview" // GPT-4 Vision
   | "gpt-4" // GPT-4
